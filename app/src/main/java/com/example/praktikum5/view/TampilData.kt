e98ca0b484a757051fc9c62abe6e6fbf69708c6c
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 15:40:35 +0700

    menambahkan title di top app bar

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -2,8 +2,14 @@ package com.example.praktikum5.view
 
 import android.R
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun FormIsian(
@@ -11,5 +17,11 @@ fun FormIsian(
     OnSubmitBtnClick : () -> Unit
 ) {
     Scaffold (modifier = Modifier,
-     )
+     topBar = {
+         TopAppBar(
+             title = { Text(stringResource(id = R.string.home),
+                 color = Color.White)},
+
+         )
+     })
 }
