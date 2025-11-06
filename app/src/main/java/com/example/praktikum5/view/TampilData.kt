f57e2f90eedbe674f2f13b850ee1559601b917d8
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 15:51:17 +0700

    tambah OutlinedTextField input Nama Lengkap

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -4,6 +4,8 @@ import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -14,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormIsian(
@@ -33,7 +36,17 @@ fun FormIsian(
         Column (modifier = Modifier.padding(isiRuang),
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {}
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+        }
 
     }
 }
