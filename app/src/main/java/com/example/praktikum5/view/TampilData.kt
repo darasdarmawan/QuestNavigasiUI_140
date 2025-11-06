f1dd6f08aa1031d16a01bdc4ee8e65da0c2e6294
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 16:17:45 +0700

    rename tampilan data jd tampil data dan tambah function tampil data

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,6 +1,11 @@
 package com.example.praktikum5.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilData(
+
+)
 
