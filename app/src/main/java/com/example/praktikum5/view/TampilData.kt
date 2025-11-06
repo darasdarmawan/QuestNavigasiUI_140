05fb4555541ec7fc593044e5a1775ff7ee223fda
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 15:36:10 +0700

    menambahkan scaffold tampilan keselurusan layar

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,6 +1,15 @@
 package com.example.praktikum5.view
 
+import android.R
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun FormIsian()
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+) {
+    Scaffold (modifier = Modifier,
+     )
+}
