7ff64a3f8d4eb66b9380121f32fbc815cad8bb5d
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 16:44:05 +0700

    tambah garis warna cyan

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -3,8 +3,10 @@ package com.example.praktikum5.view
 import android.R
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,6 +19,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -60,8 +63,9 @@ fun TampilData(
                             fontWeight = FontWeight.Bold,
                             fontFamily = FontFamily.Cursive, fontSize = 22.sp)
                     }
-
+                    HorizontalDivider(thickness = 1.dp, color = Color.Cyan)
                 }
+
             }
         }
 
