a589319e415c00199651979b05750d9e472e9d81
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 16:04:01 +0700

    ganti nama dari tampilan jd formulir

--- app/src/main/java/com/example/praktikum5/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum5/view/Formulir.kt
@@ -1,2 +1,73 @@
 package com.example.praktikum5.view
 
+import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.RadioButton
+
+@Composable
+fun FormIsian(
+    jenisK: List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick : () -> Unit
+) {
+    Scaffold (modifier = Modifier,
+     topBar = {
+         TopAppBar(
+             title = { Text(stringResource(id = R.string.home),
+                 color = Color.White)},
+             colors = TopAppBarDefaults.topAppBarColors
+                 (colorResource(id = R.color.teal_700))
+         )
+     }
+    ) { isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = {Text(text = "Nama Lengkap")},
+                onValueChange = {},
+            )
+            HorizontalDivider(modifier = Modifier
+                .padding(20.dp)
+                .width(250.dp), thickness = Thickness, color =
+                Color.Red)
+            Row {
+                jenisK.forEach {
+                    item->
+                    Row(verticalAlignment = Alignment.CenterHorizontally){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+
+        }
+
+    }
+}
