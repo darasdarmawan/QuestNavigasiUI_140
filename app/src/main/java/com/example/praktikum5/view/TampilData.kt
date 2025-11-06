1a50c376125e40695cd289567e8b3e0e933565cf
darasdarmawan <darasyauqidarmawan@gmail.com>
2025-11-06 15:47:39 +0700

    tambah padding isiRuang di Column

--- app/src/main/java/com/example/praktikum5/view/TampilData.kt
+++ app/src/main/java/com/example/praktikum5/view/TampilData.kt
@@ -1,11 +1,15 @@
 package com.example.praktikum5.view
 
 import android.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
@@ -24,5 +28,12 @@ fun FormIsian(
              colors = TopAppBarDefaults.topAppBarColors
                  (colorResource(id = R.color.teal_700))
          )
-     })
+     }
+    ) { isiRuang ->
+        Column (modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {}
+
+    }
 }
